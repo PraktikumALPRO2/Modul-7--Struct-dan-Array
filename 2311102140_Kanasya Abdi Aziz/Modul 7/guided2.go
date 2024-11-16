fe693d6bf253de9ebdafd79cb3202979b17a2e1f
asya665 <162395103+asya665@users.noreply.github.com>
2024-11-16 13:06:35 +0700

    Add files via upload

---
+++ 2311102140_Kanasya Abdi Aziz/Modul 7/guided2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sudahAda(daftarTeman []string, nama string) bool {
+	for _, teman := range daftarTeman {
+		if teman == nama {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	daftarTeman := []string{"Andi", "Budi", "Cici"}
+
+	namaBaru := []string{"Dewi", "Budi", "Eka"}
+
+	for _, nama := range namaBaru {
+		if !sudahAda(daftarTeman, nama) {
+			daftarTeman = append(daftarTeman, nama)
+		} else {
+			fmt.Println("Nama", nama, "sudah ada dalam daftar.")
+		}
+	}
+
+	fmt.Println("Daftar Teman:", daftarTeman)
+}
